4dc103e0f5ca4fef08274689b3b7beb0ca529347
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-04 12:21:18 -0300

    [fix]Config _app.js and _document.js use chackraUi

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  
+}
+
+export const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
