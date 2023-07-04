9c8b4a797caebaa4596fd9c076efe3fb9c7e3969
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-04 17:12:23 -0300

    [feat] create and render header

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { LiaDiscord } from 'react-icons/lia'
+import { AiFillGithub } from 'react-icons/ai'
+
+export const Header = () => {
+    return (
+        <Tabs variant='soft-rounded' align='center' marginTop='2' >
+            <TabList>
+                <Tab>Projects</Tab>
+                <Tab>About/Resume</Tab>
+                <Tab><LiaDiscord size={30} /></Tab>
+                <Tab><AiFillGithub size={30}></AiFillGithub></Tab>
+            </TabList>
+        </Tabs>
+    )
+}
